1f9e63d9a917d6c6f3add4af04a3a9e705b44cc8
luis <lu.enriquejv@gmail.com>
2021-09-24 14:25:23 -0500

    Corregir input de apellido

--- carrito.php
+++ carrito.php
@@ -1,34 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Carrito de compras</title>
 </head>
+
 <body>
-      <h1>Formulario</h1>
-      <form action="index.php" method="post">
-         <label>
-            Nombre completo:
-            <input type="text" name="nombre">
-         </label>
-         <br>
-         <br>
-         <label>
-            Apellido:
-            <input type="text" name="apellido">
-         </label>
-         <br>
-         <br>
-         <label>
-            Masculino
-            <input type="radio" name="genero" value="masculino">
-            Femenino
-            <input type="radio" name="genero" value="femenino">
-         </label>
-         <br>
-         <br>
-         <button type="submit">Enviar</button>
-      </form>
+   <h1>Formulario</h1>
+   <form action="index.php" method="post">
+      <label>
+         Nombre completo:
+         <input type="text" name="nombre">
+      </label>
+      <br>
+      <br>
+      <label>
+         Apellido completo:
+         <input type="text" name="apellido">
+      </label>
+      <br>
+      <br>
+      <label>
+         Masculino
+         <input type="radio" name="genero" value="masculino">
+         Femenino
+         <input type="radio" name="genero" value="femenino">
+      </label>
+      <br>
+      <br>
+      <button type="submit">Enviar</button>
+   </form>
 </body>
+
 </html>
