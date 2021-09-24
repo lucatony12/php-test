3343a97c1653b50fe803374626946bd90ed8757c
lucatony12 <lucatonyjv@gmail.com>
2021-09-24 14:39:02 -0500

    Corregir titulo

--- carrito.php
+++ carrito.php
@@ -4,7 +4,7 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Carrito de compras</title>
+   <title>Carrito de compras Plaza vea</title>
 </head>
 
 <body>
